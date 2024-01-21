42df27c743ecb5bcd165e08b8c1dbc28fa358809
Gursimran Singh Assi <ramgarhias50@gmail.com>
2024-01-21 11:16:47 -0500

    fix some issues

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,50 +1,46 @@
-import { Component ,OnInit} from '@angular/core';
-import {ContentList} from "../helper-files/content-list";
-import {Content} from "../helper-files/content-interface";
+import { Component } from '@angular/core';
+import { ContentList } from "../helper-files/content-list";
+
 
 
 @Component({
   selector: 'app-content-card',
   standalone: true,
-
   templateUrl: './content-card.component.html',
   styleUrl: './content-card.component.scss'
 })
-export class ContentCardComponent implements  OnInit{
-contentList:ContentList=new ContentList();
-contenItem1:Content={
-  id:1,
-  title:"dfdf",
-  description:"dsfgdfds",
-  creator:"dfdf",
-  imgURL:"https://angular.io/assets/images/logos/angular/logo-nav@2x.png"
-}
-  contenItem2={
-    id:2,
-    title:"dfdf",
-    description:"dsfgdfds",
-    creator:"dfdf",
-    imgURL:"https://angular.io/assets/images/logos/angular/logo-nav@2x.png"
+export class ContentCardComponent {
+  contentList: ContentList = new ContentList();
+  constructor() {
+    this.contentList.add({
+      id: 1,
+      title: "Number",
+      description: "A romantic  song",
+      creator: "Nirvair Pannu",
+      imgURL: "https://i.ytimg.com/vi/RtFFxvufwt0/maxresdefault.jpg"
+    }),
+      this.contentList.add({
+        id: 2,
+        title: "Taara Tutya",
+        description: "Its a romantic song",
+        creator: "Gur Sidhu",
+        imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvmj-_4cuFMNAPm3NzS1pJ0qQicDabvv31Rw&usqp=CAU"
+      })
+    this.contentList.add({
+      id: 3,
+      title: "Tere Layi",
+      description: "A song that show everything for you",
+      creator: "Nirvair Pannu",
+      imgURL: "https://i1.sndcdn.com/artworks-Jx9pzhvHsNl88rKr-LQbPCg-t500x500.jpg"
+    })
+    this.contentList.add({
+      id: 4,
+      title: "Apa Fer Milaange",
+      description: "About the patchup",
+      creator: "Savi Kahlon",
+      imgURL: "https://i.ytimg.com/vi/DLZD47lj82o/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD7z_p8Hf7Vug4-cyrO_1svea19QA"
+    })
   }
-  contenItem3={
-    id:3,
-    title:"dfdf",
-    description:"dsfgdfds",
-    creator:"dfdf",
-    imgURL:"https://angular.io/assets/images/logos/angular/logo-nav@2x.png"
-  }
-  contenItem4={
-    id:4,
-    title:"dfdf",
-    description:"dsfgdfds",
-    creator:"dfdf",
-    imgURL:"https://angular.io/assets/images/logos/angular/logo-nav@2x.png"
-  }
- ngOnInit() {
-  this.contentList.add(this.contenItem1);
-   this.contentList.add(this.contenItem2);
-   this.contentList.add(this.contenItem3);
-   this.contentList.add(this.contenItem4);
- }
+
 
 }
